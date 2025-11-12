77c30bc9d7b0c35359a1f0ef951e63e60e24edf7
Sean Parent <sean.parent@stlab.cc>
2025-11-11 17:36:17 -0800

    Fixing force init for CI and simplifying CI template.

--- cmake/cpp-library-ci.cmake
+++ cmake/cpp-library-ci.cmake
@@ -5,14 +5,12 @@
 # This module handles GitHub Actions workflow generation with PROJECT_NAME substitution
 
 # Function to configure CI workflow template
-function(_cpp_library_setup_ci)
-    set(options FORCE_INIT)
-    cmake_parse_arguments(ARG "${options}" "" "" ${ARGN})
-    
+# Arguments: force_init (boolean) - whether to overwrite existing file
+function(_cpp_library_setup_ci force_init)
     set(ci_template "${CPP_LIBRARY_ROOT}/templates/.github/workflows/ci.yml.in")
     set(ci_dest "${CMAKE_CURRENT_SOURCE_DIR}/.github/workflows/ci.yml")
     
-    if(EXISTS "${ci_template}" AND (NOT EXISTS "${ci_dest}" OR ARG_FORCE_INIT))
+    if(EXISTS "${ci_template}" AND (NOT EXISTS "${ci_dest}" OR force_init))
         get_filename_component(ci_dir "${ci_dest}" DIRECTORY)
         file(MAKE_DIRECTORY "${ci_dir}")
         configure_file("${ci_template}" "${ci_dest}" @ONLY)
